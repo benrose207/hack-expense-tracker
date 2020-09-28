0f7364df3d243769ed36f69d0bc675aada2732b7
Ben Rose <benrose207@gmail.com>
2020-09-27 19:29:19 -0700

    Updates

---
+++ precache-manifest.9b7f347f6b6928e14007676d6e7ecf68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246bffb1cd381ae59b27cc4f9053a546",
+    "url": "./index.html"
+  },
+  {
+    "revision": "098e78f54d229f36b732",
+    "url": "./static/css/main.74e0f5be.chunk.css"
+  },
+  {
+    "revision": "fb4b3b028a77ed2ec6b6",
+    "url": "./static/js/2.89f04cda.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "./static/js/2.89f04cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098e78f54d229f36b732",
+    "url": "./static/js/main.0cec75b0.chunk.js"
+  },
+  {
+    "revision": "a3df39f7c20a41af36b5",
+    "url": "./static/js/runtime-main.d8f5974d.js"
+  }
+]);
